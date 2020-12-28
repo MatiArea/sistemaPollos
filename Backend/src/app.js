c819671e98fd29101f208d6638906060d3ecc9ab
Matias Agustin Area <40324614+MatiArea@users.noreply.github.com>
2020-12-27 23:05:16 -0300

    Update app.js

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -50,7 +50,7 @@ app.use((req, res, next) => {
   app.use("/report", ReportRouter);
   //
   app.get("/*", function (req, res) {
-    res.sendFile("./public/index.html");
+    res.sendFile("/home/bitnami/sistemaPollos/Backend/public/index.html");
   });
   
   export default app;
