a3dbc81390bdede120b8665a27aec31679158186
Matias Agustin Area <40324614+MatiArea@users.noreply.github.com>
2021-02-12 18:54:18 -0300

    Hotfix host and port connection

--- src/index.js
+++ src/index.js
@@ -1,20 +1,25 @@
 import app from "./app";
 import "@babel/polyfill";
-import sequelize from './BaseDeDatos/database'
+import sequelize from "./BaseDeDatos/database";
 
 async function main() {
-    const port = 4000
-    await app.listen(port);
-    console.log('Server on port ',port);
-    try {
-        await sequelize.sync({ force: false }).then(() => {
-            console.log("Base de datos conectada");
-        }).catch(error => {
-            console.log('Se ha producido un error', error);
-        })        
-    } catch (error) {
-        console.log({"Error":error})
-    }
+  var port = process.env.PORT || 80;
+  var host = process.env.HOST || "0.0.0.0";
+  try {
+    await app.listen(port, host, function () {
+      console.log("Listening on port %d", port);
+    });
+    await sequelize
+      .sync({ force: false })
+      .then(() => {
+        console.log("Database connected");
+      })
+      .catch((error) => {
+        console.log("Se ha producido un error", error);
+      });
+  } catch (error) {
+    console.log({ Error: error });
+  }
 }
 
 main();
